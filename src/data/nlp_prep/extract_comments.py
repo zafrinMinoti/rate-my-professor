daa8f22ee3edd4ca5a07f666b52996676da78024
Zafrin Minoti <zafrin.minoti@gmail.com>
2018-07-04 23:14:41 -0400

    commit before deleting previous functions from extract_comments.py

--- src/data/nlp_prep/extract_comments.py
+++ src/data/nlp_prep/extract_comments.py
@@ -1,5 +1,6 @@
 from spacy import load
 nlp = load('en')
+from multiprocessing import Pool
 
 def extract_comments_sents():
     '''
@@ -10,6 +11,7 @@ def extract_comments_sents():
 
     raw = '../RateMyProfessor/data/raw/reviews.json'
     intermi = '../RateMyProfessor/data/intermediate/comments/'
+    processed = '../RateMyProfessor/data/intermediate/processed/'
 
     with open(raw, 'r') as raw_file, \
             open(intermi+'comment_sents_tracker.txt', 'w') as tracker, \
@@ -48,6 +50,148 @@ def extract_comments():
             tracker.write('{},{}\n'.format(indict['prof_id'], indict['reviewer_id']))
             comments.write('{}\n'.format(indict['comments']))
 
+def write_raw_comments(raw_comment_list, outfile):
+    outfile.write('llll')
+
+def write_raw_comment_sents(raw_comment_sent_list, outfile):
+    pass
+
+def write_tokanized_comment(raw_comment_list, outfile):
+    pass
+
+def mainnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn():
+    count = 0
+
+    prof_ids = []
+    reviewer_ids = []
+    comments = []
+
+    pr_sent_ids = []
+    sents = []
+
+    tokenized_comments = []
+    tokenized_sents = []
+
+    with open(raw, 'r') as raw_file, \
+            open(intermi + 'comment_tracker.txt', 'w') as comment_tracker_file, \
+            open(intermi + 'comment_raw.txt', 'w') as comment_raw_file, \
+            open(intermi + 'comment_sents_tracker.txt', 'w') as sent_tracker_file, \
+            open(intermi + 'comment_sents_raw.txt', 'w') as sent_raw_file, \
+            open(processed + 'comment_tokenized_lemmatized.txt', 'w') as comment_token_file, \
+            open(processed + 'sent_tokenized_lemmatized.txt', 'w') as sent_token_file:
+
+
+        indict = raw_file.readline()[:-2]
+        # print(indict)
+        # print(type(indict))
+        indict = eval(indict)
+        # print(type(indict))
+
+        comment = indict['comments']
+        #
+        parsed_comment = nlp(comment)
+
+        # separate raw comments and treck them
+        prof_ids.append(indict['prof_id'])
+        reviewer_ids.append(indict['reviewer_id'])
+        comments.append(comment)
+
+        # tokenize comments and separate them
+        tokenized_comments.append([token.lemma_ for token in parsed_comment])
+
+        # separate sentences and track them
+        for num, sent in enumerate(parsed_comment.sents):
+            pr_sent_ids.append(zip(indict['prof_id'], indict['reviewer_id'], num))
+            sents.append(sent)
+            # tokenize sentences and separate them
+            tokenized_sents.append([token.lemma_ for token in sent])
+
+
+        # Write to files on batches
+        count += 1
+
+        if count == 1000:
+            for i, comment in enumerate(comments):
+                comment_raw_file.write(comment)
+                comment_tracker_file.write(zip(prof_ids[i], reviewer_ids[i]))
+                comment_token_file.write(tokenized_comments[i])
+
+            for i, sent in enumerate(sents):
+                sent_raw_file.write(sent)
+                sent_tracker_file.write(pr_sent_ids)
+                sent_token_file.wr
+
+            count = 0
+
+
+
+
+
+
+def main():
+    raw = '../RateMyProfessor/data/raw/reviews.json'
+    intermi = '../RateMyProfessor/data/intermediate/comments/'
+    processed = '../RateMyProfessor/data/processed/'
+
+    count = 0
+
+    prof_ids = ''
+    reviewer_ids = ''
+    comments = ''
+
+    pr_sent_ids = ''
+    sents = ''
+
+    tokenized_comments = ''
+    tokenized_sents = ''
+
+    with open(raw, 'r') as raw_file, \
+            open(intermi + 'comment_tracker.txt', 'w') as comment_tracker_file, \
+            open(intermi + 'comment_raw.txt', 'w') as comment_raw_file, \
+            open(intermi + 'comment_sents_tracker.txt', 'w') as sent_tracker_file, \
+            open(intermi + 'comment_sents_raw.txt', 'w') as sent_raw_file, \
+            open(processed + 'comment_tokenized_lemmatized.txt', 'w') as comment_token_file, \
+            open(processed + 'sent_tokenized_lemmatized.txt', 'w') as sent_token_file:
+
+        for line in raw_file:
+            indict = line[:-2]
+            # print(type(indict))
+            indict = eval(indict)
+            # print(type(indict))
+
+            comment = indict['comments']
+            prof_id = indict['prof_id']
+            reviewer_id = indict['reviewer_id']
+            #
+            parsed_comment = nlp(comment)
+
+            # separate raw comments and treck them
+            prof_ids += str(prof_id) + ',' + str(reviewer_id) + '\n'
+            comments += comment + '\n'
+
+            # tokenize comments and separate them
+            tokenized_comments += str(([token.lemma_ for token in parsed_comment])) + '\n'
+
+            # # separate sentences and track them
+            # for num, sent in enumerate(parsed_comment.sents):
+            #     pr_sent_ids.append(zip(indict['prof_id'], indict['reviewer_id'], num))
+            #     sents.append(sent)
+            #     # tokenize sentences and separate them
+            #     tokenized_sents.append([token.lemma_ for token in sent])
+
+
+            # Write to files on batches
+            count += 1
+            # print(comment)
+
+            if count%1000 == 0:
+                comment_raw_file.write(comments)
+                comment_tracker_file.write(prof_ids)
+                comment_token_file.write(tokenized_comments)
+                print('{} DONE!!!'.format(count))
+
+
 if __name__ == '__main__':
-    extract_comments_sents()
-    extract_comments()
+    # extract_comments_sents()
+    # extract_comments()
+    main()
